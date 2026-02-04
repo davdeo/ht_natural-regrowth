c6869c28974bbab0ee07795c33d0862a7fc6eab6
davdeo <david.monz@hotmail.com>
2026-02-04 14:03:05 +0100

    Initial setup of project with examples

--- src/main/java/com/davdeo/naturalregrowthplugin/NaturalRegrowthPlugin.java
+++ src/main/java/com/davdeo/naturalregrowthplugin/NaturalRegrowthPlugin.java
@@ -1,13 +1,14 @@
-package com.example.exampleplugin;
+package com.davdeo.naturalregrowthplugin;
 
+import com.davdeo.naturalregrowthplugin.events.ExampleBreakBlockSystem;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
-public class ExamplePlugin extends JavaPlugin {
+public class NaturalRegrowthPlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public ExamplePlugin(JavaPluginInit init) {
+    public NaturalRegrowthPlugin(JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
     }
@@ -15,5 +16,6 @@ public class ExamplePlugin extends JavaPlugin {
     @Override
     protected void setup() {
         this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+        this.getEntityStoreRegistry().registerSystem(new ExampleBreakBlockSystem());
     }
 }
